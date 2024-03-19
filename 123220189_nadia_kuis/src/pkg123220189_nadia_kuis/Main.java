a761d1e4bd638c259d03bc4698e31b49c97b7fa9
NadiaNurHalisa <161995535+NadiaNurHalisa@users.noreply.github.com>
2024-03-19 09:57:28 +0700

    Add files via upload

---
+++ 123220189_nadia_kuis/src/pkg123220189_nadia_kuis/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg123220189_nadia_kuis;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+inport java.swing.*;
+inport java.swt.*;
+inport java.swt.event.*;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
